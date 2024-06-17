31b1cca54973961612d0ec680fc6ee0e9806f494
Casper121 <juanjosejimenez00@gmail.com>
2024-06-17 13:56:22 +0200

    first steps

---
+++ react-app/data/User.ts
@@ -0,0 +1,5 @@
+export type User = {
+    name: string;
+    lastname: string;
+    age: number;
+}
---
+++ react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const App = () => {
+
+    const text = "Hello World"
+
+    return(
+        <div>
+            {text}
+        </div>
+    )
+
+}
+
+export default App
